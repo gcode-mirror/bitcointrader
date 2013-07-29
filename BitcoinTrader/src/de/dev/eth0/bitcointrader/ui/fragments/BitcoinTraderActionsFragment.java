b18fa146120a2cb4a4c0316eef13ef6e323af794
deveth0 <deveth0@08b35d1c-6bd9-4155-951d-087fba4d2d0d>
2013-07-29 14:47:47 +0000

    Bitcointrader: fixes #107
    git-svn-id: svn://localhost/opt/svn/bitcointrader/Code/Privat/Android/BitcoinTrader/trunk@221 08b35d1c-6bd9-4155-951d-087fba4d2d0d

--- BitcoinTrader/src/de/dev/eth0/bitcointrader/ui/fragments/BitcoinTraderActionsFragment.java
+++ BitcoinTrader/src/de/dev/eth0/bitcointrader/ui/fragments/BitcoinTraderActionsFragment.java
@@ -4,22 +4,16 @@ package de.dev.eth0.bitcointrader.ui.fragments;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 import com.xeiam.xchange.dto.Order;
-import de.dev.eth0.bitcointrader.Constants;
 import de.dev.eth0.bitcointrader.R;
 import de.dev.eth0.bitcointrader.ui.PlaceOrderActivity;
-import de.dev.eth0.bitcointrader.ui.StartScreenActivity;
 
 /**
  * @author Alexander Muthmann
@@ -54,23 +48,6 @@ public class BitcoinTraderActionsFragment extends Fragment {
         Intent i = new Intent(activity, PlaceOrderActivity.class);
         i.putExtra(PlaceOrderActivity.INTENT_EXTRA_TYPE, Order.OrderType.ASK.name());
         startActivity(i);
-      }
-    });
-
-    final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(activity);
-    TextView demoInfoView = (TextView) view.findViewById(R.id.bitcointrader_demo_info);
-    demoInfoView.setText(R.string.bitcoin_action_demo_info);
-    demoInfoView.setVisibility(prefs.getBoolean(Constants.PREFS_KEY_DEMO, false) ? View.VISIBLE : View.GONE);
-
-    demoInfoView.setOnClickListener(new OnClickListener() {
-      public void onClick(final View v) {
-        Editor editor = prefs.edit();
-        editor.putString(Constants.PREFS_KEY_MTGOX_APIKEY, null);
-        editor.putString(Constants.PREFS_KEY_MTGOX_SECRETKEY, null);
-        editor.putBoolean(Constants.PREFS_KEY_DEMO, false);
-        editor.commit();
-        startActivity(new Intent(activity, StartScreenActivity.class));
-        activity.finish();
       }
     });
     return view;
