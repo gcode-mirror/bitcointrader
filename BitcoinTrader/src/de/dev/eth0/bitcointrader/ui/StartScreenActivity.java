1897788c3cf10b9b17a8445e662f7fe4323e03e9
deveth0 <deveth0@08b35d1c-6bd9-4155-951d-087fba4d2d0d>
2013-10-06 09:27:25 +0000

    Bitcointrader: fixes #139 (reportscreen on all activities)
    git-svn-id: svn://localhost/opt/svn/bitcointrader/Code/Privat/Android/BitcoinTrader/trunk@305 08b35d1c-6bd9-4155-951d-087fba4d2d0d

--- BitcoinTrader/src/de/dev/eth0/bitcointrader/ui/StartScreenActivity.java
+++ BitcoinTrader/src/de/dev/eth0/bitcointrader/ui/StartScreenActivity.java
@@ -18,9 +18,6 @@ import android.util.Log;
 import android.view.Window;
 import de.dev.eth0.bitcointrader.R;
 import de.dev.eth0.bitcointrader.Constants;
-import de.dev.eth0.bitcointrader.util.CrashReporter;
-import de.schildbach.wallet.ui.ReportIssueDialogBuilder;
-import java.io.IOException;
 
 /**
  * @author Alexander Muthmann
@@ -33,17 +30,12 @@ public class StartScreenActivity extends AbstractBitcoinTraderActivity {
   private BroadcastReceiver broadcastReceiver;
   private LocalBroadcastManager broadcastManager;
   private AlertDialog alertDialog;
-  private boolean hadErrors = false;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     requestWindowFeature(Window.FEATURE_NO_TITLE);
     setContentView(R.layout.start_activity);
-    hadErrors = false;
-    if (savedInstanceState == null) {
-      checkAlerts();
-    }
   }
 
   @Override
@@ -114,75 +106,5 @@ public class StartScreenActivity extends AbstractBitcoinTraderActivity {
     mDialog.setCancelable(false);
     mDialog.show();
     getBitcoinTraderApplication().startExchangeService();
-  }
-
-  private void checkAlerts() {
-    if (CrashReporter.hasSavedCrashTrace()) {
-      final StringBuilder stackTrace = new StringBuilder();
-      final StringBuilder applicationLog = new StringBuilder();
-
-      try {
-        hadErrors = true;
-        CrashReporter.appendSavedCrashTrace(stackTrace);
-        CrashReporter.appendSavedCrashApplicationLog(applicationLog);
-      } catch (final IOException x) {
-        Log.w(TAG, "IO Exception", x);
-      }
-
-      final ReportIssueDialogBuilder dialog = new ReportIssueDialogBuilder(this, R.string.report_issue_dialog_title_crash,
-              R.string.report_issue_dialog_message_crash) {
-        @Override
-        protected void onReportFinished() {
-          hadErrors = false;
-          onResume();
-        }
-
-        @Override
-        protected CharSequence subject() {
-          try {
-            if (CrashReporter.hasReason()) {
-              return CrashReporter.getReason();
-            }
-          } catch (IOException ioe) {
-            Log.w(TAG, "Exception", ioe);
-          }
-          return Constants.REPORT_SUBJECT_CRASH + " " + getBitcoinTraderApplication().applicationVersionName();
-        }
-
-        @Override
-        protected CharSequence collectApplicationInfo() throws IOException {
-          final StringBuilder applicationInfo = new StringBuilder();
-          CrashReporter.appendApplicationInfo(applicationInfo, getBitcoinTraderApplication());
-          return applicationInfo;
-        }
-
-        @Override
-        protected CharSequence collectStackTrace() throws IOException {
-          if (stackTrace.length() > 0) {
-            return stackTrace;
-          } else {
-            return null;
-          }
-        }
-
-        @Override
-        protected CharSequence collectDeviceInfo() throws IOException {
-          final StringBuilder deviceInfo = new StringBuilder();
-          CrashReporter.appendDeviceInfo(deviceInfo, StartScreenActivity.this);
-          return deviceInfo;
-        }
-
-        @Override
-        protected CharSequence collectApplicationLog() throws IOException {
-          if (applicationLog.length() > 0) {
-            return applicationLog;
-          } else {
-            return null;
-          }
-        }
-      };
-
-      dialog.show();
-    }
   }
 }
